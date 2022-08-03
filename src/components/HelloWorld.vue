68ff7e09d7c7f43141194bfea5a31ae4df25c617
jijunkang <1148862830@qq.com>
2022-08-03 16:03:22 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+  <!--1.数据绑定最常见的形式就是使用 {{...}}（双大括号）的文本插值：-->
+  <h1>{{ msg }}</h1>
+  <hr>
+
+  <!--    2.使用 v-html 指令用于输出 html 代码：-->
+  <div v-html="msg"></div>
+  <hr>
+
+  <!--    3.HTML 属性中的值应使用 v-bind 指令。以下实例判断 use 的值，如果为 true 使用 class1 类的样式，否则不使用该类：-->
+  <label for="r1">修改颜色</label><input type="checkbox" v-model="use" id="r1">
+  <div v-bind:class="{'class1': use}">
+    v-bind:class 指令
   </div>
+  <hr>
+
+  <!--  4.指令是带有 v- 前缀的特殊属性。指令用于在表达式的值改变时，将某些行为应用到 DOM 上。如下例子：-->
+  <label>显示</label><input type="checkbox" v-model="seen">
+  <p v-if="seen">现在你看到我了</p>
+  <hr>
+
+  <!--  5.参数在指令后以冒号指明。例如， v-bind 指令被用来响应地更新 HTML 属性：-->
+  <a v-bind:href="url">菜鸟教程</a>
+  <hr>
+
+  <!--  6.在 input 输入框中我们可以使用 v-model 指令来实现双向数据绑定：-->
+  <p>{{ msg }}</p>
+  <input v-model="msg">
+  <hr>
+
+  <!--  7.v-model 指令用来在 input、select、textarea、checkbox、radio 等表单控件元素上创建双向数据绑定，根据表单上的值，自动更新绑定的元素的值。
+    按钮的事件我们可以使用 v-on 监听事件，并对用户的输入进行响应。
+    以下实例在用户点击按钮后对字符串进行反转操作：-->
+  <p>{{ message }}</p>
+  <button v-on:click="reverseMessage">反转字符串</button>
+  <hr>
+
+  <!--  8.过滤器函数接受表达式的值作为第一个参数。
+    以下实例对输入的字符串第一个字母转为大写：-->
+  <!--  <p>{{ message | capitalize }}</p>
+    <hr>-->
+
+  <!--  循环-->
+<!--  <ul>
+    <template v-for="site in sites">
+      <li>{{ site.name }}</li>
+    </template>
+  </ul>-->
+
+<!--  计算属性computed-->
+  <label>计算属性computed，反转字符</label>{{reversedMessage}}
+  <hr>
+
+
+
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      msg: 'vue从入门到放弃！',
+      message: 'pompom',
+      use: false,
+      seen: true,
+      url: 'http://www.runoob.com',
+      sites: [
+        { name: 'Runoob' },
+        { name: 'Google' },
+        { name: 'Taobao' }
+      ]
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  },
+  filters: {
+    capitalize: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  },
+  computed: {
+    // 计算属性的 getter
+    reversedMessage: function () {
+      // `this` 指向 vm 实例
+      return this.message.split('').reverse().join('')
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.class1 {
+  background: #444;
+  color: #eee;
 }
 </style>
---
+++ src/views/qibu.vue
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Vue 测试实例 - 菜鸟教程(runoob.com)</title>
+  <script src="https://cdn.staticfile.org/vue/2.4.2/vue.min.js"></script>
+</head>
+<body>
+<!--
+可以看到在 Vue 构造器中有一个el 参数，它是 DOM 元素中的 id。在上面实例中 id 为 vue_det，在 div 元素中：
+
+<div id = "vue_det"></div>
+这意味着我们接下来的改动全部在以上指定的 div 内，div 外部不受影响。
+
+接下来我们看看如何定义数据对象。
+
+data 用于定义属性，实例中有三个属性分别为：site、url、alexa。
+
+methods 用于定义的函数，可以通过 return 来返回函数值。
+
+{{ }} 用于输出对象属性和函数返回值。-->
+<div id="vue_det">
+  <h1>site : {{site}}</h1>
+  <h1>url : {{url}}</h1>
+  <h1>{{details()}}</h1>
+</div>
+<script type="text/javascript">
+  var vm = new Vue({
+    el: '#vue_det',
+    data: {
+      site: "菜鸟教程",
+      url: "www.runoob.com",
+      alexa: "10000"
+    },
+    methods: {
+      details: function() {
+        return  this.site + " - 学的不仅是技术，更是梦想！";
+      }
+    }
+  })
+</script>
+</body>
+</html>
+
+
