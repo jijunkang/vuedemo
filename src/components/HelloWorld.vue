ccf27a15d1608be06b098bb95bbff5cf0d86f230
jijunkang <1148862830@qq.com>
2022-08-03 17:37:47 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,17 +42,38 @@
     <hr>-->
 
   <!--  循环-->
-<!--  <ul>
-    <template v-for="site in sites">
-      <li>{{ site.name }}</li>
-    </template>
-  </ul>-->
+  <!--  <ul>
+      <template v-for="site in sites">
+        <li>{{ site.name }}</li>
+      </template>
+    </ul>-->
 
-<!--  计算属性computed-->
-  <label>计算属性computed，反转字符</label>{{reversedMessage}}
+  <!--  计算属性computed-->
+  <label>计算属性computed，反转字符</label>{{ reversedMessage }}
   <hr>
 
 
+  监听属性
+  <div id="computed_props">
+    千米 : <input type="text" v-model="kilometers">
+    米 : <input type="text" v-model="meters">
+  </div>
+  <hr>
+
+  class 属性绑定
+  <div v-bind:class="classObject"></div>
+  <hr>
+
+  Vue.js style(内联样式)
+  <div v-bind:style="{ color: activeColor, fontSize: fontSize + 'px' }">菜鸟教程</div>
+  <div v-bind:style="styleObject">菜鸟教程</div>
+  <div v-bind:style="[baseStyles, overridingStyles]">菜鸟教程</div>
+  <hr>
+
+  Vue.js 事件处理器
+  <button v-on:click="kilometers += 1">增加 1</button>
+  <p>这个按钮被点击了 {{ kilometers }} 次。</p>
+  <button v-on:click="greet">Greet</button>
 
 </template>
 
@@ -66,16 +87,45 @@ export default {
       use: false,
       seen: true,
       url: 'http://www.runoob.com',
+      kilometers: 0,
+      meters: 0,
+      isActive: true,
+      error: {
+        value: true,
+        type: 'fatal'
+      },
       sites: [
-        { name: 'Runoob' },
-        { name: 'Google' },
-        { name: 'Taobao' }
-      ]
+        {name: 'Runoob'},
+        {name: 'Google'},
+        {name: 'Taobao'}
+      ],
+      activeColor: 'green',
+      fontSize: 30,
+      styleObject: {
+        color: 'green',
+        fontSize: '30px'
+      },
+      baseStyles: {
+        color: 'green',
+        fontSize: '30px'
+      },
+      overridingStyles: {
+        'font-weight': 'bold'
+      }
+
     }
   },
   methods: {
     reverseMessage: function () {
       this.message = this.message.split('').reverse().join('')
+    },
+    greet: function (event) {
+      // `this` 在方法里指当前 Vue 实例
+      alert('Hello ' + this.msg + '!')
+      // `event` 是原生 DOM 事件
+      if (event) {
+        alert(event.target.tagName)
+      }
     }
   },
   filters: {
@@ -90,6 +140,24 @@ export default {
     reversedMessage: function () {
       // `this` 指向 vm 实例
       return this.message.split('').reverse().join('')
+    },
+    //返回一个class对象
+    classObject: function () {
+      return {
+        base: true,
+        active: this.isActive && !this.error.value,
+        'text-danger': this.error.value && this.error.type === 'fatal',
+      }
+    }
+  },
+  watch: {
+    kilometers: function (val) {
+      this.kilometers = val;
+      this.meters = this.kilometers * 1000
+    },
+    meters: function (val) {
+      this.kilometers = val / 1000;
+      this.meters = val;
     }
   }
 }
@@ -99,5 +167,19 @@ export default {
 .class1 {
   background: #444;
   color: #eee;
+}
+
+.base {
+  width: 100px;
+  height: 100px;
+  margin: auto;
+}
+
+.active {
+  background: green;
+}
+
+.text-danger {
+  background: red;
 }
 </style>
